c40ca5ac81b9f72bfb3eb016c6d548461c212614
FedeHide <larcdian@gmail.com>
2025-01-10 19:50:11 -0300

    fix: move FeatureGroup creation to create_map method due to overcreation of fg in create_marker method

--- main.py
+++ main.py
@@ -20,6 +20,21 @@ MARKERS = [
 
 
 class Map:
+    """
+    A class to represent a map.
+
+    Attributes:
+        location (list): The center of the map.
+        zoom_start (int): The initial zoom level.
+        tiles (str): The style of the map tiles.
+        markers (list): A list of markers to add to the map.
+
+    Methods:
+        create_map(): Create a map with the given location, zoom level, and tiles style. Add markers to the map.
+        create_marker(location: list, popup: str, icon: folium.Icon): Create a marker with the given location, popup, and icon.
+        save(filename: str): Save the map to a file.
+    """
+
     def __init__(
         self,
         location: list = INITIAL_LOCATION,
@@ -33,6 +48,18 @@ class Map:
         self.markers = markers
 
     def create_map(self) -> folium.Map:
+        """
+        Create a map with the given location, zoom level, and tiles style. Add markers to the map.
+
+        Args:
+            location (list): The center of the map.
+            zoom_start (int): The initial zoom level.
+            tiles (str): The style of the map tiles.
+            markers (list): A list of markers to add to the map.
+
+        Returns:
+            folium.Map: A map object with the given location, zoom level, tiles style, and markers.
+        """
         try:
             map = folium.Map(
                 location=self.location, zoom_start=self.zoom_start, tiles=self.tiles
@@ -45,8 +72,13 @@ class Map:
                 location=INITIAL_LOCATION, zoom_start=3, tiles=FALLBACK_TILES_STYLE
             )
 
+        # FeatureGroup is a container for multiple features
+        # TODO: Add a feature group for each type of marker (??)
+        # TODO: check naming conventions for feature groups
+        markers_fg = folium.FeatureGroup(name="My Map")
+
         for marker in self.markers:
-            map.add_child(
+            markers_fg.add_child(
                 self.create_marker(
                     location=marker["location"],
                     popup=marker["popup"],
@@ -54,23 +86,39 @@ class Map:
                 )
             )
 
+        map.add_child(markers_fg)
+
+        # Add a layer control to the map
+        map.add_child(folium.LayerControl())
+
         return map
 
     def create_marker(self, location: list, popup: str, icon: folium.Icon):
-        # FeatureGroup is a container for multiple features
-        # TODO: Add a feature group for each type of marker (??)
-        # TODO: check naming conventions for feature groups
-        markers_fg = folium.FeatureGroup(name="My Map")
+        """
+        Create a marker with the given location, popup, and icon.
 
-        # Create a marker
+        Args:
+            location (list): The location of the marker.
+            popup (str): The text to display when the marker is clicked.
+            icon (folium.Icon): The icon to use for the marker.
+
+        Returns:
+            folium.Marker: A marker object with the given location, popup, and icon.
+        """
+
         marker = folium.Marker(location=location, popup=popup, icon=icon)
 
-        # Add marker to the feature group
-        markers_fg.add_child(marker)
-
-        return markers_fg
+        # Create a marker
+        return marker
 
     def save(self, filename: str):
+        """
+        Save the map to a file.
+
+        Args:
+            filename (str): The name of the file to save the map to.
+        """
+
         self.create_map().save(filename)
 
 
